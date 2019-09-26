59ed9e3dcd368fed74118b48b9aafb5ec24fbb89
Reilly Chase <rchase@rchase.com>
2019-09-26 18:48:56 -0400

    Update unifi-installer.sh

--- unifi-installer.sh
+++ unifi-installer.sh
@@ -14,35 +14,29 @@ echo $HOSTNAMEVAR;
 apt-get update && apt-get upgrade -y
 
 echo "Installing swap"
-sleep 2
 fallocate -l 1G /swapfile
 chmod 600 /swapfile
 echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
 echo "vm.swappiness=10" >> /etc/sysctl.conf
 
 echo "Installing support tools"
-sleep 2
 apt-get install apt-transport-https -y
 apt-get install ncdu -y
 apt-get install htop -y
 
 echo "Setting hostname"
-sleep 2
 hostname $HOSTNAMEVAR
 hostnamectl set-hostname $HOSTNAMEVAR
 
 echo "Installing UniFi"
-sleep 2
 wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ubnt.com/unifi/unifi-repo.gpg 
 echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | tee /etc/apt/sources.list.d/100-ubnt-unifi.list
 apt-get update && apt-get install unifi -y
 
 echo "Installing NGINX"
-sleep 2
 apt-get install nginx-light -y
 
 echo "Installing Let's Encrypt"
-sleep 2
 echo "deb http://ftp.debian.org/debian stretch-backports main" | tee -a /etc/apt/sources.list
 apt-get update -y
 apt-get install python-certbot-nginx -t stretch-backports -y
@@ -52,14 +46,12 @@ chmod +x /root/unifi-lets-encrypt-ssl-importer.sh
 /root/unifi-lets-encrypt-ssl-importer.sh -d $HOSTNAMEVAR
 
 echo "Creating Let's Encrypt cron"
-sleep 2
 crontab -l > /root/letsencryptcron
 echo "0 23 * * * /bin/bash /root/unifi-lets-encrypt-ssl-importer.sh -d $HOSTNAMEVAR" >> /root/letsencryptcron
 crontab /root/letsencryptcron
 rm /root/letsencryptcron
 
 echo "Creating firewall rules"
-sleep 2
 iptables -t nat -I PREROUTING -p tcp --dport 443 -j REDIRECT --to-ports 8443
 iptables -A INPUT -i ens3 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
@@ -74,16 +66,13 @@ iptables -A INPUT -j DROP
 iptables -A OUTPUT -o ens3 -j ACCEPT
 
 echo "Saving firewall rules"
-sleep 2
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
 apt-get install iptables-persistent netfilter-persistent -y
 
-sleep 2
 netfilter-persistent save
 
 echo "Configuring NGINX to forward HTTP to HTTPS"
-sleep 2
 echo "server {
     listen 80 default_server;
     listen [::]:80 default_server;
@@ -94,7 +83,6 @@ ln -s /etc/nginx/sites-available/redirect /etc/nginx/sites-enabled/
 rm /etc/nginx/sites-enabled/default
 
 echo "Restarting services"
-sleep 2
 systemctl restart nginx
 systemctl restart unifi
 
